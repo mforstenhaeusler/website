604918de341b0728fc4528fab56e5cefb9f83f5b
Maximilian Forstenhäusler <mforstenhaeusler@Maximilians-MacBook-Pro.local>
2024-02-29 15:53:56 +0100

    adds google analystics tag

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from '../src/components/themeProvider';
 //import { ScrollContainer, ScrollPage } from 'react-scroll-motion';
+import Script from 'next/script'
 
 export const metadata = {
   title: 'M. Forstenäusler',
@@ -20,6 +21,16 @@ export default function RootLayout({
         <link rel="icon" href="headshot/favicon.jpg" className='rounded-full'></link>
         {/*<meta name="viewport" content="width=device-width, height=device-height"></meta>*/}
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        
+        {/*<!-- Google Tag Manager -->*/}
+        <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WNKKFJF4');`}
+        </Script>
+        {/*<!-- End Google Tag Manager -->*/}
       </head>
         <body id="body" className="flex h-full w-screen bg-secondary overflow-x-hidden">
           <div className='overflow-x-hidden'> 
@@ -38,7 +49,17 @@ export default function RootLayout({
               </div>
             </ThemeProvider>
           </div>
-          
+          {/*<!-- Google Tag Manager (noscript) -->*/}
+          <noscript>
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WNKKFJF4"
+              height="0" 
+              width="0" 
+              style={{display: "none", visibility: "hidden"}}
+            >
+            </iframe>
+          </noscript>
+          {/*<!-- End Google Tag Manager (noscript) -->*/}
 
         </body>
       {/*</ScrollContainer>*/}
